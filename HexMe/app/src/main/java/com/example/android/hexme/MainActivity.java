89632f34a6ea865afa45c975d18c7fb5837d832d
Ryan <ryan.samarajeewa@gmail.com>
2015-06-18 13:18:47 -0700

    Remove action bar

--- HexMe/app/src/main/java/com/example/android/hexme/MainActivity.java
+++ HexMe/app/src/main/java/com/example/android/hexme/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.hexme;
 
 import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,6 +19,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 
     @Override
