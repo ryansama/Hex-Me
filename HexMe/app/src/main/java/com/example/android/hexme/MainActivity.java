251aa50b9d18fe55bf1deb134241bfbb29b70efa
Ryan <ryan.samarajeewa@gmail.com>
2015-06-18 13:05:41 -0700

    Create, position and style views

--- HexMe/app/src/main/java/com/example/android/hexme/MainActivity.java
+++ HexMe/app/src/main/java/com/example/android/hexme/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -36,5 +39,47 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void changeColor(View view){
+        String randColor = "#"+ genValue()+genValue()+genValue();
+    } 
+    /**
+     *Generates a string of two characters that
+     *correspont to a hexadecimal value from 00 to FF
+     *Three of these strings represent
+     *a color combining red, green, and blue (RGB).
+     *@return a hex value from 00 to FF
+     */
+    private static String genValue(){
+        String[] letters = {"A", "B", "C", "D", "E", "F"};//array of letters for hexadecimal value
 
+        boolean digitZero = getRandomBoolean();//if true, the 0th digit of the hex number is a letter (A to F), and a number (0-9) if false
+        boolean digitOne = getRandomBoolean();//if true, the 1st digit of the hex number is a letter (A to F), and a number (0-9) if false
+
+        String toReturn;//String object to be returned
+
+        //determine if the 0th digit is a letter or a number
+        if(digitOne){
+            toReturn = letters[(int)(Math.random()*5)];
+        }else{
+            toReturn = Integer.toString((int)(Math.random()*9));
+        }
+
+        //determine if the 1st digit is a letter or a number
+        if(digitZero){
+            toReturn = toReturn + letters[(int)(Math.random()*5)];
+        }else{
+            toReturn = toReturn + Integer.toString((int)(Math.random()*9));
+        }
+
+        return toReturn;
+    }
+
+    /**
+     *Generates a random boolean value.
+     *@return a random boolean (true or false)
+     */
+    private static boolean getRandomBoolean() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
 }
